7f65ca96ff57eb13f9795f7f14afb719e278fb88
Asasay <blackjewrobot@gmail.com>
2025-01-28 22:17:36 +0300

    rm tailwind config

--- app/components/featureCard.tsx
+++ app/components/featureCard.tsx
@@ -8,7 +8,7 @@ interface FeatureCardProps extends React.ButtonHTMLAttributes<HTMLButtonElement>
 
 const FeatureCard = ({ title, description, icon }: FeatureCardProps) => {
   return (
-    <article className="flex flex-col justify-between gap-[60px] h-full p-4 bg-[#F0F0F0] transition-shadow hover:shadow-card rounded-md lg:p-6 lg:block">
+    <article className="flex flex-col justify-between gap-[60px] h-full p-4 bg-[#F0F0F0] transition-shadow hover:shadow-card rounded-lg lg:p-6 lg:block">
       <header className="flex place-items-center gap-6 lg:flex-col lg:items-start lg:gap-0 lg:h-full lg:justify-between">
         <Image src={icon} alt="feature icon" width={60} height={60} className="w-auto lg:h-20" />
         <h2 className="flex flex-wrap items-center [overflow-wrap:anywhere] text-2xl/7 lg:text-[32px]/9">
